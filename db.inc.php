7d73e5ca915716d86bcdbfbe2a8f4821fce42801
lyfewithcode <lyfewithcode@gmail.com>
2021-07-16 10:38:03 +0700

    Selecting database using mysql

--- db.inc.php
+++ db.inc.php
@@ -2,9 +2,12 @@
     $host = "localhost";
     $user = "root";
     $password = '';
+    $db = "company";
+
+    $connect = @mysqli_connect($host, $user, $password);
     
-    if(@mysqli_connect($host, $user, $password)) { // adding "@" symbol to hiding server error responeses
-        // echo 'connected';
+    if(@mysqli_select_db($connect, $db)) {
+        // echo 'db selected';
     } else {
         echo 'oops... not connected';
     }
